f55ace02009313617bcf9cebd208a5a824c6be4a
mfkfawas <mfkfawas@gmail.com>
2022-03-01 11:12:41 +0530

    finishing the bookings API

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,13 +4,27 @@ const authController = require('../controllers/authController')
 
 const router = express.Router()
 
+router.use(authController.protect)
+
 //This route is not following the rest principle, this route is only for the clients to get a checkout session
 router.get(
   //we want client to send along the ID of tour that is currently being booked, so we can fill up the checkout session with all the data that is
   //necessary such as tour name,...
   '/checkout-session/:tourId',
-  authController.protect,
   bookingController.getCheckoutSession
 )
+
+router.use(authController.restrictTo('admin', 'lead-guide'))
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking)
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking)
 
 module.exports = router
